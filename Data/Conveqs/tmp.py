522c7adec85343e64a85616296ff19b02ded882c
Satyam Agarwal <thesatyam@outlook.com>
2024-08-22 14:33:03 +0000

    Update Data/ directory

---
+++ Data/Conveqs/tmp.py
@@ -0,0 +1,9 @@
+import pandas as pd
+data = pd.read_csv('./conveqs.csv')
+print(data)
+
+data = pd.read_csv('./conveqs_large.csv')
+print(data)
+
+data = pd.read_csv('./conveqs_small.csv')
+print(data)
